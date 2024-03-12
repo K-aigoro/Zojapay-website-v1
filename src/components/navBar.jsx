433a28c1956af889c0950c0363e16193123fe74a
K-aigoro <aigorokazeem90@gmail.com>
2024-03-12 13:12:42 +0100

    new update

---
+++ src/components/navBar.jsx
@@ -0,0 +1,41 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+// import Logo from '@/assets/image/zojapay.png"';
+// import Logo from '@/assets/image/zojapay.png';
+import Logo from '../assets/Image/zojapay.png'
+import './navbar.css';
+
+
+function Navbar() {
+  return (
+    <header>
+      <div className="header">
+        {/* Company logo */}
+        <div className="logo">
+          <img src={Logo} alt="Zojapay" />
+        </div>
+
+        {/* Features & FAQS */}
+        <div className="nav">
+          <ul className="list">
+            <li>
+              <a href="/"> Features</a>
+            </li>
+            <li>
+              <a href="/"> FAQS </a>
+            </li>
+          </ul>
+        </div>
+
+        {/* Signup */}
+        <div className="">
+            <button className="button">
+                Sign up
+            </button>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Navbar;
