a53b0c322b5fa694b452664b2d550a5f3c6a9906
K-aigoro <aigorokazeem90@gmail.com>
2024-03-12 13:26:57 +0100

    hero page

---
+++ src/components/hero.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+ function Hero() {
+  return (
+    <section className='hero'>
+
+    </section>
+  );
+}
+
+export default Hero;
