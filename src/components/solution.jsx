35f698c01b7f7ce903156b9a9768f32fa458ae17
K-aigoro <aigorokazeem90@gmail.com>
2024-03-13 12:48:04 +0100

    implement solution comp

---
+++ src/components/solution.jsx
@@ -0,0 +1,33 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+function Solution() {
+  return (
+    <div className="">
+      <section>
+        <h2 className="h1">One-stop solution for all payments</h2>
+        <div className="">
+          <article className="">
+            <div>
+              <img src="" alt="" />
+            </div>
+            <div className="">
+              <div>
+                <img src="" alt="" />
+              </div>
+              <h2>Contactless Payments Made Easy</h2>
+              <p>
+                Our platform supports QR code payments, allowing customers to
+                simply scan and pay, and NFC (Tap to Pay) technology, make quick
+                and secure payments by simply tapping their smartphones or
+                contactless cards, providing a seamless payment experience.
+              </p>
+            </div>
+          </article>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Solution;
